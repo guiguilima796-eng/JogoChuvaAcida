42611766b3cf3b6927c44dd3d611dba780f78a50
Gui <guiguilima796@gmail.com>
2025-10-06 17:06:11 -0300

    desenhando um jogador

--- main.py
+++ main.py
@@ -8,11 +8,16 @@ pygame.display.set_caption('Chuva ácida')
 
 relogio = pygame.time.Clock()
 
+jogador = pygame.Rect((largura//2) - 15,altura -30,30,30)
+
+
 rodando = True 
 
 while rodando:
     for evento in pygame.event.get():
         if evento.type == pygame.QUIT:
             rodando = False
+    
+    pygame.draw.rect(tela,(0,255,0),jogador)
 
-
+pygame.quit()
