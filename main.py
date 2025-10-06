846563bae90193317f4520baf150a9ed286616af
Gui <guiguilima796@gmail.com>
2025-10-06 17:31:23 -0300

    terceiro commit

--- main.py
+++ main.py
@@ -18,6 +18,24 @@ while rodando:
         if evento.type == pygame.QUIT:
             rodando = False
     
+    # imagem = __file__('imgs/dowload.png')
+    teclas = pygame.key.get_pressed()
+
+    if teclas[pygame.K_LEFT]:
+        if jogador.x >= 3:
+            jogador.x -= 3
+    if teclas[pygame.K_RIGHT]:
+        if jogador.x <= largura - 3:
+            jogador.x += 3
+    if teclas[pygame.K_DOWN]:
+        if jogador.y <= altura - 3:
+            jogador.y += 3
+    if teclas[pygame.K_AT]:
+        if jogador.y >= 3:
+            jogador.y += 3
+    tela.fill((30,30,30))    
     pygame.draw.rect(tela,(0,255,0),jogador)
+    pygame.display.flip()
+    pygame.time.Clock().tick(60)
 
 pygame.quit()
