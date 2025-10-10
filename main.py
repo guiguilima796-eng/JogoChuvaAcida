c773004e6d702005870dddbf6d0161262c3d079c
Gui <guiguilima796@gmail.com>
2025-10-10 15:44:46 -0300

    outras modificações

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 pygame.init()
 
@@ -8,16 +9,26 @@ pygame.display.set_caption('Chuva ácida')
 
 relogio = pygame.time.Clock()
 
-jogador = pygame.Rect((largura//2) - 15,altura -30,30,30)
 
 
+def reiniciar_jogo():
+    jogador = pygame.Rect((largura//2) - 15,altura -30,30,30)
+    obstaculos = []
+    pontuacao = 0
+    return jogador,obstaculos,pontuacao
+
+jogador,obstaculos,pontuacao = reiniciar_jogo()
+fonte = pygame.font.Font(None,30)
 rodando = True 
+game_over = False
 
 while rodando:
+
     for evento in pygame.event.get():
         if evento.type == pygame.QUIT:
             rodando = False
     
+
     # imagem = __file__('imgs/dowload.png')
     teclas = pygame.key.get_pressed()
 
@@ -25,17 +36,39 @@ while rodando:
         if jogador.x >= 3:
             jogador.x -= 3
     if teclas[pygame.K_RIGHT]:
-        if jogador.x <= largura - 3:
+        if jogador.x <= (largura - jogador.width):
             jogador.x += 3
-    if teclas[pygame.K_DOWN]:
-        if jogador.y <= altura - 3:
-            jogador.y += 3
-    if teclas[pygame.K_AT]:
-        if jogador.y >= 3:
-            jogador.y += 3
+    # if teclas[pygame.K_DOWN]:
+    #     if jogador.y <= (altura - jogador.width):
+    #         jogador.y += 3
+    # if teclas[pygame.K_]:
+    #     if jogador.y >= (altura + 3):
+    #         jogador.y += 3
+
+    if(random.randint(1,20) == 1):
+        obstaculos.append(pygame.Rect(random.randint(0,largura - 30),0,30,30))
+
+    for obstaculo in obstaculos:
+            obstaculo.y +=8
+            if obstaculo.colliderect(jogador):
+                rodando = False
+
+    
     tela.fill((30,30,30))    
+
     pygame.draw.rect(tela,(0,255,0),jogador)
+
+    for obstaculo in obstaculos:
+        pygame.draw.rect(tela,(255,0,0),obstaculo)
+    
+    obstaculos = [obstaculo for obstaculo in obstaculos if obstaculo.y < altura]
+
+
+
     pygame.display.flip()
+
     pygame.time.Clock().tick(60)
+
+
 
 pygame.quit()
